6c905e417a32f69b35325f134f9e063fe7965d9d
listenzcc <cc.public@outlook.com>
2023-11-23 14:43:14 +0800

    Init commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# EEG for everyone
+
+---
+- [EEG for everyone](#eeg-for-everyone)
+  - [How to use](#how-to-use)
+  - [Function](#function)
+  - [Visualization](#visualization)
+  - [References](#references)
+
+## How to use
+
+---
+
+## Function
+
+## Visualization
+
+## References
---
+++ sys_info.py
@@ -0,0 +1,37 @@
+"""
+File: sys_info.py
+Author: Chuncheng Zhang
+Date: 2023-11-23
+Copyright & Email: chuncheng.zhang@ia.ac.cn
+
+Purpose:
+    Test and list required modules and assets.
+
+Functions:
+    1. Requirements and constants
+    2. Function and class
+    3. Play ground
+    4. Pending
+    5. Pending
+"""
+
+
+# %% ---- 2023-11-23 ------------------------
+# Requirements and constants
+from rich import print, inspect
+
+
+# %% ---- 2023-11-23 ------------------------
+# Function and class
+
+
+# %% ---- 2023-11-23 ------------------------
+# Play ground
+
+
+# %% ---- 2023-11-23 ------------------------
+# Pending
+
+
+# %% ---- 2023-11-23 ------------------------
+# Pending
